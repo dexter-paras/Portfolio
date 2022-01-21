e264aa21cff10da96bc160cf9fbacebf1379ed99
Paras Chawla <parchawl@amazon.com>
2022-01-21 22:39:13 +0530

    adding some images

--- index.js
+++ index.js
@@ -27,3 +27,40 @@ function doSomethingCool(){
 	}
 
 }
+
+var arr=['Paras','Sonal','Aman','Rashi'];
+
+function generateRandomColor()
+{
+    var randomColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+    return randomColor;
+    //random color will be freshly served
+}
+
+function animation(id){
+				const letters = id.innerText.split('');
+
+				// 2. For every character, we want <span class="js-letter">P</span><span class="js-letter">a</span>
+				// Every Letter is having 2 classes one is letter-> css and other one js-letter-> java script
+				let html = "";
+				letters.forEach(letter => {
+					let classes = ''
+					if(letter != ' '){
+						classes ='letter js-letter'
+					}
+					html = html + `<span class='${classes}'>${letter}</span>`
+				})
+
+				id.innerHTML=html;
+
+			// 2. Add EventListeners in mouse over and mouse Out
+			const letterNode = document.querySelectorAll('.js-letter');
+			letterNode.forEach(node =>{
+				node.addEventListener('mouseover',function(event){
+					this.classList.add("active");
+				})
+				node.addEventListener('mouseout',function(event){
+					this.classList.remove("active");
+				})
+			})
+}
